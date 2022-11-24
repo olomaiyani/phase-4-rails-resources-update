785a7c9c457880cc491946c1d29d9199e893ee7d
Nana <nanaolomaiyani@gmail.com>
2022-11-24 21:59:35 +0300

    assignment done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,17 +1,14 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
-
   # POST /birds
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -22,10 +19,31 @@ class BirdsController < ApplicationController
     end
   end
 
+  def update 
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: {error: 'Bird not found'}, status: :not_found
+    end 
+  end
+
+  def increment_likes
+    bird = Bird.find(params[:id])
+    if bird 
+      bird.update(likes:bird.likes + 1)
+      render json: bird 
+    else 
+      render json: {error: 'Bird not found'}, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
     params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
